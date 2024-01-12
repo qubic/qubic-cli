06694f1316da2200c03dd9083238cfee17b24eef
Qsilver97 <152714424+Qsilver97@users.noreply.github.com>
2024-01-12 12:58:09 +0300

    Update nodeUtils.h: add missing definition

--- nodeUtils.h
+++ nodeUtils.h
@@ -3,6 +3,7 @@ void printTickInfoFromNode(const char* nodeIp, int nodePort);
 uint32_t getTickNumberFromNode(const char* nodeIp, int nodePort);
 bool checkTxOnTick(const char* nodeIp, const int nodePort, const char* txHash, uint32_t requestedTick);
 void getTickDataToFile(const char* nodeIp, const int nodePort, uint32_t requestedTick, const char* fileName);
+int32_t getQuorumTickToFile(const char* nodeIp, const int nodePort, uint32_t requestedTick, const char* fileName);
 void printTickDataFromFile(const char* fileName, const char* compFile);
 bool checkTxOnFile(const char* txHash, const char* fileName);
 void sendRawPacket(const char* nodeIp, const int nodePort, int rawPacketSize, uint8_t* rawPacket);
