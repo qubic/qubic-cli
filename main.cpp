96f7026b6a80b5acb24c09bf550d165e99c5dc26
JOETOM <joetom@qubic.li>
2024-03-05 21:26:40 +0100

    tab to space convert

--- main.cpp
+++ main.cpp
@@ -13,60 +13,60 @@
 
 int main(int argc, char *argv[])
 {
-	parseArgument(argc, argv);
-	switch (g_cmd){
-		case SHOW_KEYS:
+    parseArgument(argc, argv);
+    switch (g_cmd){
+        case SHOW_KEYS:
             sanityCheckSeed(g_seed);
-			printWalletInfo(g_seed);
-			break;
-		case GET_CURRENT_TICK:
-			sanityCheckNode(g_nodeIp, g_nodePort);
-			printTickInfoFromNode(g_nodeIp, g_nodePort);
-			break;
+            printWalletInfo(g_seed);
+            break;
+        case GET_CURRENT_TICK:
+            sanityCheckNode(g_nodeIp, g_nodePort);
+            printTickInfoFromNode(g_nodeIp, g_nodePort);
+            break;
         case GET_SYSTEM_INFO:
-			sanityCheckNode(g_nodeIp, g_nodePort);
-			printSystemInfoFromNode(g_nodeIp, g_nodePort);
-			break;
-		case GET_BALANCE:
-			sanityCheckIdentity(g_requestedIdentity);
-			sanityCheckNode(g_nodeIp, g_nodePort);
-			printBalance(g_requestedIdentity, g_nodeIp, g_nodePort);
-			break;
-		case GET_ASSET:
+            sanityCheckNode(g_nodeIp, g_nodePort);
+            printSystemInfoFromNode(g_nodeIp, g_nodePort);
+            break;
+        case GET_BALANCE:
+            sanityCheckIdentity(g_requestedIdentity);
+            sanityCheckNode(g_nodeIp, g_nodePort);
+            printBalance(g_requestedIdentity, g_nodeIp, g_nodePort);
+            break;
+        case GET_ASSET:
             sanityCheckIdentity(g_requestedIdentity);
             sanityCheckNode(g_nodeIp, g_nodePort);
             printOwnedAsset(g_nodeIp, g_nodePort, g_requestedIdentity);
             printPossessionAsset(g_nodeIp, g_nodePort, g_requestedIdentity);
-			break;
-		case SEND_COIN:
-			sanityCheckNode(g_nodeIp, g_nodePort);
+            break;
+        case SEND_COIN:
+            sanityCheckNode(g_nodeIp, g_nodePort);
             sanityCheckSeed(g_seed);
-			sanityCheckIdentity(g_targetIdentity);
-			sanityCheckTxAmount(g_TxAmount);
-			makeStandardTransaction(g_nodeIp, g_nodePort, g_seed, g_targetIdentity, g_TxAmount, g_offsetScheduledTick, g_waitUntilFinish);
-			break;
-		case SEND_CUSTOM_TX:
-			sanityCheckNode(g_nodeIp, g_nodePort);
+            sanityCheckIdentity(g_targetIdentity);
+            sanityCheckTxAmount(g_TxAmount);
+            makeStandardTransaction(g_nodeIp, g_nodePort, g_seed, g_targetIdentity, g_TxAmount, g_offsetScheduledTick, g_waitUntilFinish);
+            break;
+        case SEND_CUSTOM_TX:
+            sanityCheckNode(g_nodeIp, g_nodePort);
             sanityCheckSeed(g_seed);
             sanityCheckTxType(g_TxType);
-			sanityCheckIdentity(g_targetIdentity);
-			sanityCheckTxAmount(g_TxAmount);
-			sanityCheckExtraDataSize(g_txExtraDataSize);
+            sanityCheckIdentity(g_targetIdentity);
+            sanityCheckTxAmount(g_TxAmount);
+            sanityCheckExtraDataSize(g_txExtraDataSize);
             makeCustomTransaction(g_nodeIp, g_nodePort, g_seed, g_targetIdentity,
                                   g_TxType, g_TxAmount, g_txExtraDataSize,
                                   g_txExtraData, g_offsetScheduledTick);
 
-			break;
+            break;
         case CHECK_TX_ON_TICK:
             sanityCheckNode(g_nodeIp, g_nodePort);
             sanityCheckTxHash(g_requestedTxId);
             checkTxOnTick(g_nodeIp, g_nodePort, g_requestedTxId, g_requestedTickNumber);
             break;
-		case SEND_RAW_PACKET:
-			sanityCheckNode(g_nodeIp, g_nodePort);
-			sanityCheckRawPacketSize(g_rawPacketSize);
-			sendRawPacket(g_nodeIp, g_nodePort, g_rawPacketSize, g_rawPacket);
-			break;
+        case SEND_RAW_PACKET:
+            sanityCheckNode(g_nodeIp, g_nodePort);
+            sanityCheckRawPacketSize(g_rawPacketSize);
+            sendRawPacket(g_nodeIp, g_nodePort, g_rawPacketSize, g_rawPacket);
+            break;
         case GET_TICK_DATA:
             sanityCheckNode(g_nodeIp, g_nodePort);
             getTickDataToFile(g_nodeIp, g_nodePort, g_requestedTickNumber, g_requestedFileName);
@@ -85,12 +85,12 @@ int main(int argc, char *argv[])
             sanityCheckTxHash(g_requestedTxId);
             checkTxOnFile(g_requestedTxId, g_requestedFileName);
             break;
-		case PUBLISH_PROPOSAL:
-			printf("On development. Come back later\n");
-			break;
-		case VOTE_PROPOSAL:
-			printf("On development. Come back later\n");
-			break;
+        case PUBLISH_PROPOSAL:
+            printf("On development. Come back later\n");
+            break;
+        case VOTE_PROPOSAL:
+            printf("On development. Come back later\n");
+            break;
         case QX_ISSUE_ASSET:
             sanityCheckNode(g_nodeIp, g_nodePort);
             sanityCheckSeed(g_seed);
@@ -231,9 +231,9 @@ int main(int argc, char *argv[])
             sanityFileExist(g_qutil_sendtomanyv1_payout_list_file);
             qutilSendToManyV1(g_nodeIp, g_nodePort, g_seed, g_qutil_sendtomanyv1_payout_list_file, g_offsetScheduledTick);
             break;
-		default:
-			printf("Unexpected command!\n");
-			break;
-	}
+        default:
+            printf("Unexpected command!\n");
+            break;
+    }
     return 0;
 }
