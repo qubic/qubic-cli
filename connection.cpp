ac3a83efffe83b14a7e7d428b1c7c366a03cfec3
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2024-11-20 16:58:28 +0100

    better formatting

--- connection.cpp
+++ connection.cpp
@@ -147,7 +147,7 @@ T QubicConnection::receivePacketWithHeaderAs(bool skipOtherHeaders)
     
     int packetSize = header.size();
     int remainingSize = packetSize - sizeof(RequestResponseHeader);
-    LOG(("Receiving remaining " + std::to_string(remainingSize) + " bytes...").c_str());
+    LOG(("Receiving remaining " + std::to_string(remainingSize) + " bytes...\n").c_str());
     T result;
     memset(&result, 0, sizeof(T));
     if (remainingSize)
