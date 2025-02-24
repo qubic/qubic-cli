8ae59ae814cda4e07776c4136d7ef3461194623c
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-02-24 17:24:21 +0100

    order variables in output struct according to size

--- testUtils.cpp
+++ testUtils.cpp
@@ -5,7 +5,7 @@
 
 // Change this index when new contracts are added
 #define TESTEXA_CONTRACT_INDEX 12
-#define TESTEXA_ADDRESS ""
+#define TESTEXA_ADDRESS "MAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAWLWD"
 
 // TESTEXA FUNCTIONS
 constexpr uint8_t TESTEXA_QUERY_QPI_FUNCTIONS = 1;
@@ -26,160 +26,173 @@ constexpr uint8_t TESTEXA_QUERY_QPI_FUNCTIONS_TO_STATE = 7;
 void testQpiFunctionsBeginAndEndTick(const char* nodeIp, const int nodePort)
 {
     // get current tick
-	auto qc = make_qc(nodeIp, nodePort);
-	uint32_t currentTick = getTickNumberFromNode(qc);
+    auto qc = make_qc(nodeIp, nodePort);
+    uint32_t currentTick = getTickNumberFromNode(qc);
 
-	// request output of qpi functions that were saved at begin and end tick for the last 16 ticks
-	for (int tickOffset = 0; tickOffset < 16; ++tickOffset)
-	{
-		QpiFunctionsOutput beginTickOutput, endTickOutput;
+    // request output of qpi functions that were saved at begin and end tick for the last 16 ticks
+    for (int tickOffset = 0; tickOffset < 16; ++tickOffset)
+    {
+        QpiFunctionsOutput beginTickOutput, endTickOutput;
+        struct {
+            RequestResponseHeader header;
+            RequestContractFunction rcf;
+            uint32_t tick;
+        } packetBegin;
+        packetBegin.header.setSize(sizeof(packetBegin));
+        packetBegin.header.randomizeDejavu();
+        packetBegin.header.setType(RequestContractFunction::type());
+        packetBegin.rcf.inputSize = sizeof(uint32_t);
+        packetBegin.rcf.contractIndex = TESTEXA_CONTRACT_INDEX;
+        uint32_t requestedTick = currentTick - tickOffset;
+        packetBegin.tick = requestedTick;
 
-		struct {
-			RequestResponseHeader header;
-			RequestContractFunction rcf;
-			uint32_t tick;
-		} packet;
-		packet.header.setSize(sizeof(packet));
-		packet.header.randomizeDejavu();
-		packet.header.setType(RequestContractFunction::type());
-		packet.rcf.inputSize = sizeof(uint32_t);
-		packet.rcf.contractIndex = TESTEXA_CONTRACT_INDEX;
-		uint32_t requestedTick = currentTick - tickOffset;
-		packet.tick = requestedTick;
+        packetBegin.rcf.inputType = TESTEXA_RETURN_QPI_FUNCTIONS_OUTPUT_BEGIN_TICK;
+        qc->sendData((uint8_t*)&packetBegin, packetBegin.header.size());
+        try
+        {
+            beginTickOutput = qc->receivePacketWithHeaderAs<QpiFunctionsOutput>();
+            LOG("beginTickOutput for tick %u received\n", beginTickOutput.tick);
+        }
+        catch (std::logic_error& e)
+        {
+            memset(&beginTickOutput, 0, sizeof(beginTickOutput));
+        }
 
-		packet.rcf.inputType = TESTEXA_RETURN_QPI_FUNCTIONS_OUTPUT_BEGIN_TICK;
-		qc->sendData((uint8_t*)&packet, packet.header.size());
-		try
-		{
-			beginTickOutput = qc->receivePacketWithHeaderAs<QpiFunctionsOutput>();
-		}
-		catch (std::logic_error& e)
-		{
-			memset(&beginTickOutput, 0, sizeof(beginTickOutput));
-		}
+        struct {
+            RequestResponseHeader header;
+            RequestContractFunction rcf;
+            uint32_t tick;
+        } packetEnd;
+        packetEnd.header.setSize(sizeof(packetEnd));
+        packetEnd.header.randomizeDejavu();
+        packetEnd.header.setType(RequestContractFunction::type());
+        packetEnd.rcf.inputSize = sizeof(uint32_t);
+        packetEnd.rcf.contractIndex = TESTEXA_CONTRACT_INDEX;
+        packetEnd.tick = requestedTick;
+        packetEnd.header.randomizeDejavu();
+        packetEnd.rcf.inputType = TESTEXA_RETURN_QPI_FUNCTIONS_OUTPUT_END_TICK;
+        qc->sendData((uint8_t*)&packetEnd, packetEnd.header.size());
+        try
+        {
+            endTickOutput = qc->receivePacketWithHeaderAs<QpiFunctionsOutput>();
+            LOG("endTickOutput for tick %u received\n", endTickOutput.tick);
+        }
+        catch (std::logic_error& e)
+        {
+            memset(&endTickOutput, 0, sizeof(endTickOutput));
+        }
 
-		packet.rcf.inputType = TESTEXA_RETURN_QPI_FUNCTIONS_OUTPUT_END_TICK;
-		qc->sendData((uint8_t*)&packet, packet.header.size());
-		try
-		{
-			endTickOutput = qc->receivePacketWithHeaderAs<QpiFunctionsOutput>();
-		}
-		catch (std::logic_error& e)
-		{
-			memset(&endTickOutput, 0, sizeof(endTickOutput));
-		}
-
-		LOG("Tick %u\n", requestedTick);
-		if (beginTickOutput.tick == requestedTick && endTickOutput.tick == requestedTick)
-		{
-			LOG("\t- year: ");
-			if (beginTickOutput.year == endTickOutput.year)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.year, endTickOutput.year);
-			LOG("\t- month: ");
-			if (beginTickOutput.month == endTickOutput.month)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.month, endTickOutput.month);
-			LOG("\t- day: ");
-			if (beginTickOutput.day == endTickOutput.day)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.day, endTickOutput.day);
-			LOG("\t- hour: ");
-			if (beginTickOutput.hour == endTickOutput.hour)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.hour, endTickOutput.hour);
-			LOG("\t- minute: ");
-			if (beginTickOutput.minute == endTickOutput.minute)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.minute, endTickOutput.minute);
-			LOG("\t- second: ");
-			if (beginTickOutput.second == endTickOutput.second)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.second, endTickOutput.second);
-			LOG("\t- millisecond: ");
-			if (beginTickOutput.millisecond == endTickOutput.millisecond)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.millisecond, endTickOutput.millisecond);
-			LOG("\t- dayOfWeek: ");
-			if (beginTickOutput.dayOfWeek == endTickOutput.dayOfWeek)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.dayOfWeek, endTickOutput.dayOfWeek);
-			LOG("\t- arbitrator: ");
-			bool matches = true;
-			int i;
-			for (i = 0; i < 32; ++i)
-				if (beginTickOutput.arbitrator[i] != endTickOutput.arbitrator[i])
-				{
-					matches = false;
-					break;
-				}
-			if (matches)
-				LOG("matches\n");
-			else
-				LOG("differs in byte %d! (beginTick %u, endTick %u)\n", i, beginTickOutput.arbitrator[i], endTickOutput.arbitrator[i]);
-			LOG("\t- computor0: ");
-			matches = true;
-			for (i = 0; i < 32; ++i)
-				if (beginTickOutput.computor0[i] != endTickOutput.computor0[i])
-				{
-					matches = false;
-					break;
-				}
-			if (matches)
-				LOG("matches\n");
-			else
-				LOG("differs in byte %d! (beginTick %u, endTick %u)\n", i, beginTickOutput.computor0[i], endTickOutput.computor0[i]);
-			LOG("\t- epoch: ");
-			if (beginTickOutput.epoch == endTickOutput.epoch)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.epoch, endTickOutput.epoch);
-			LOG("\t- invocationReward: ");
-			if (beginTickOutput.invocationReward == endTickOutput.invocationReward)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %lld, endTick %lld)\n", beginTickOutput.invocationReward, endTickOutput.invocationReward);
-			LOG("\t- invocator: ");
-			matches = true;
-			for (i = 0; i < 32; ++i)
-				if (beginTickOutput.invocator[i] != endTickOutput.invocator[i])
-				{
-					matches = false;
-					break;
-				}
-			if (matches)
-				LOG("matches\n");
-			else
-				LOG("differs in byte %d! (beginTick %u, endTick %u)\n", i, beginTickOutput.invocator[i], endTickOutput.invocator[i]);
-			LOG("\t- numberOfTickTransactions: ");
-			if (beginTickOutput.numberOfTickTransactions == endTickOutput.numberOfTickTransactions)
-				LOG("matches\n");
-			else
-				LOG("differs! (beginTick %d, endTick %d)\n", beginTickOutput.numberOfTickTransactions, endTickOutput.numberOfTickTransactions);
-			LOG("\t- originator: ");
-			matches = true;
-			for (i = 0; i < 32; ++i)
-				if (beginTickOutput.originator[i] != endTickOutput.originator[i])
-				{
-					matches = false;
-					break;
-				}
-			if (matches)
-				LOG("matches\n");
-			else
-				LOG("differs in byte %d! (beginTick %u, endTick %u)\n", i, beginTickOutput.originator[i], endTickOutput.originator[i]);
-		}
-		else
-		{
-			LOG("\tfailed to get qpi functions output\n");
-		}
-	}
+        LOG("Tick %u\n", requestedTick);
+        if (beginTickOutput.tick == requestedTick && endTickOutput.tick == requestedTick)
+        {
+            LOG("\t- year: ");
+            if (beginTickOutput.year == endTickOutput.year)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.year, endTickOutput.year);
+            LOG("\t- month: ");
+            if (beginTickOutput.month == endTickOutput.month)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.month, endTickOutput.month);
+            LOG("\t- day: ");
+            if (beginTickOutput.day == endTickOutput.day)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.day, endTickOutput.day);
+            LOG("\t- hour: ");
+            if (beginTickOutput.hour == endTickOutput.hour)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.hour, endTickOutput.hour);
+            LOG("\t- minute: ");
+            if (beginTickOutput.minute == endTickOutput.minute)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.minute, endTickOutput.minute);
+            LOG("\t- second: ");
+            if (beginTickOutput.second == endTickOutput.second)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.second, endTickOutput.second);
+            LOG("\t- millisecond: ");
+            if (beginTickOutput.millisecond == endTickOutput.millisecond)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.millisecond, endTickOutput.millisecond);
+            LOG("\t- dayOfWeek: ");
+            if (beginTickOutput.dayOfWeek == endTickOutput.dayOfWeek)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.dayOfWeek, endTickOutput.dayOfWeek);
+            LOG("\t- arbitrator: ");
+            bool matches = true;
+            int i;
+            for (i = 0; i < 32; ++i)
+                if (beginTickOutput.arbitrator[i] != endTickOutput.arbitrator[i])
+                {
+                    matches = false;
+                    break;
+                }
+            if (matches)
+                LOG("matches\n");
+            else
+                LOG("differs in byte %d! (beginTick %u, endTick %u)\n", i, beginTickOutput.arbitrator[i], endTickOutput.arbitrator[i]);
+            LOG("\t- computor0: ");
+            matches = true;
+            for (i = 0; i < 32; ++i)
+                if (beginTickOutput.computor0[i] != endTickOutput.computor0[i])
+                {
+                    matches = false;
+                    break;
+                }
+            if (matches)
+                LOG("matches\n");
+            else
+                LOG("differs in byte %d! (beginTick %u, endTick %u)\n", i, beginTickOutput.computor0[i], endTickOutput.computor0[i]);
+            LOG("\t- epoch: ");
+            if (beginTickOutput.epoch == endTickOutput.epoch)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %u, endTick %u)\n", beginTickOutput.epoch, endTickOutput.epoch);
+            LOG("\t- invocationReward: ");
+            if (beginTickOutput.invocationReward == endTickOutput.invocationReward)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %lld, endTick %lld)\n", beginTickOutput.invocationReward, endTickOutput.invocationReward);
+            LOG("\t- invocator: ");
+            matches = true;
+            for (i = 0; i < 32; ++i)
+                if (beginTickOutput.invocator[i] != endTickOutput.invocator[i])
+                {
+                    matches = false;
+                    break;
+                }
+            if (matches)
+                LOG("matches\n");
+            else
+                LOG("differs in byte %d! (beginTick %u, endTick %u)\n", i, beginTickOutput.invocator[i], endTickOutput.invocator[i]);
+            LOG("\t- numberOfTickTransactions: ");
+            if (beginTickOutput.numberOfTickTransactions == endTickOutput.numberOfTickTransactions)
+                LOG("matches\n");
+            else
+                LOG("differs! (beginTick %d, endTick %d)\n", beginTickOutput.numberOfTickTransactions, endTickOutput.numberOfTickTransactions);
+            LOG("\t- originator: ");
+            matches = true;
+            for (i = 0; i < 32; ++i)
+                if (beginTickOutput.originator[i] != endTickOutput.originator[i])
+                {
+                    matches = false;
+                    break;
+                }
+            if (matches)
+                LOG("matches\n");
+            else
+                LOG("differs in byte %d! (beginTick %u, endTick %u)\n", i, beginTickOutput.originator[i], endTickOutput.originator[i]);
+        }
+        else
+        {
+            LOG("\tfailed to get qpi functions output\n");
+        }
+    }
 }
--- testUtils.h
+++ testUtils.h
@@ -4,27 +4,27 @@
 
 struct QpiFunctionsOutput
 {
-	uint8_t year;   // [0..99] (0 = 2000, 1 = 2001, ..., 99 = 2099)
-	uint8_t month;  // [1..12]
-	uint8_t day;    // [1..31]
-	uint8_t hour;   // [0..23]
-	uint8_t minute; // [0..59]
-	uint8_t second;
-	uint16_t millisecond;
-	uint8_t dayOfWeek;
-	uint8_t arbitrator[32];
-	uint8_t computor0[32];
-	uint16_t epoch;
-	int64_t invocationReward;
-	uint8_t invocator[32];
-	int32_t numberOfTickTransactions;
-	uint8_t originator[32];
-	uint32_t tick;
+    uint8_t arbitrator[32];
+    uint8_t computor0[32];
+    uint8_t invocator[32];
+    uint8_t originator[32];
+    int64_t invocationReward;
+    int32_t numberOfTickTransactions;
+    uint32_t tick;
+    uint16_t epoch;
+    uint16_t millisecond;
+    uint8_t year;   // [0..99] (0 = 2000, 1 = 2001, ..., 99 = 2099)
+    uint8_t month;  // [1..12]
+    uint8_t day;    // [1..31]
+    uint8_t hour;   // [0..23]
+    uint8_t minute; // [0..59]
+    uint8_t second;
+    uint8_t dayOfWeek;
 
-	static constexpr unsigned char type()
-	{
-		return RespondContractFunction::type();
-	}
+    static constexpr unsigned char type()
+    {
+        return RespondContractFunction::type();
+    }
 };
 
 void testQpiFunctionsBeginAndEndTick(const char* nodeIp, const int nodePort);
